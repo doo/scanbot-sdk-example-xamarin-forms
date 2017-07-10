a911838b9dcd06c829c248c22be8e41c591d8e09
Eduard Frank <eddy@scanbot.io>
2017-07-10 15:05:58 +0200

    initial commit

---
+++ scanbot-sdk-example-xamarin-forms/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace scanbotsdkexamplexamarinforms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new scanbot_sdk_example_xamarin_formsPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ scanbot-sdk-example-xamarin-forms/scanbot-sdk-example-xamarin-formsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace scanbotsdkexamplexamarinforms
+{
+    public partial class scanbot_sdk_example_xamarin_formsPage : ContentPage
+    {
+        public scanbot_sdk_example_xamarin_formsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

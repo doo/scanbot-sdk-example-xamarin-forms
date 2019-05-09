abbec5cfe29191efdf873fff1d4924ea9dc31246
Eduard Frank <eddy@scanbot.io>
2019-05-09 17:54:12 +0200

    Change some configs for testing purposes.

--- Droid/MainApplication.cs
+++ Droid/MainApplication.cs
@@ -20,7 +20,26 @@ namespace scanbotsdkexamplexamarinforms.Droid
         // However, if you do not specify the license key when initializing the SDK, 
         // it will work in trial mode (trial period of 1 minute). 
         // To get another trial period you have to restart your app.
-        const string licenseKey = null;
+        //const string licenseKey = null;
+
+        // limited trial key
+        const string licenseKey =
+              "ToNwgEItnq6POs/fCbMlXxJ+CiEG3y" +
+              "m/p4zxOnydigb3IJaJ1mcFc0M7Uytx" +
+              "wfCerGJFsmtFXaLJAX/dlkCg00/hMj" +
+              "859KG1WTvuI4fnxHKyolYcG6HUVn+G" +
+              "n/uw3DaAN5aw3ScLTAAXHWQpubrRgy" +
+              "wEHtW6LGNKlr6+Cr6g6s//3oO2ulqc" +
+              "3f8LxtVRoJOLi9GHH7gysFDgjGSSHG" +
+              "Nd7d99OaoRQeqJFoo5POOcKSPJnP9H" +
+              "M8NNGI0P0WtQp+d6ABh61triMT4q5P" +
+              "VR8rk9OSYP+El9MLEtDNxlKRpJcqfB" +
+              "Cy93R/JtlMz4sUQk9M1WLB2eDyAIcw" +
+              "zv9Ixj3LEs+A==\nU2NhbmJvdFNESw" +
+              "ppby5zY2FuYm90LmV4YW1wbGUuc2Nh" +
+              "bmJvdF9zZGtfZXhhbXBsZV94YW1hcm" +
+              "luX2Zvcm1zCjE1NjAxMjQ3OTkKMTMx" +
+              "MDcxCjI=\n";
 
         public MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
         { }
@@ -42,7 +61,7 @@ namespace scanbotsdkexamplexamarinforms.Droid
         {
             // !! Please note !!
             // In this demo app we overwrite the "StorageBaseDirectory" of the Scanbot SDK by a custom public(!) storage directory.
-            // "Android.OS.Environment.ExternalStorageDirectory" is an external, public(!) storage directoy.
+            // "GetExternalFilesDir" returns an external, public(!) storage directoy.
             // All image files as well export files (PDF, TIFF, etc) created by the Scanbot SDK in this demo app will be stored 
             // in a sub-folder of this storage directory and will be accessible for every(!) app having external storage permissions!
             // We use the "ExternalStorageDirectory" here only for demo purposes, to be able to share generated PDF and TIFF files. 
@@ -55,8 +74,7 @@ namespace scanbotsdkexamplexamarinforms.Droid
             // - https://developer.android.com/guide/topics/data/data-storage
             // - https://docs.microsoft.com/en-us/xamarin/android/platform/files/
 
-            var externalPublicPath = Path.Combine(
-                Android.OS.Environment.ExternalStorageDirectory.Path, "scanbot-sdk-example-xamarin-forms_demo-storage");
+            var externalPublicPath = Path.Combine(GetExternalFilesDir(null).AbsolutePath, "my-custom-storage");
             Directory.CreateDirectory(externalPublicPath);
             return externalPublicPath;
         }

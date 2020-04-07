d1766067d9710b5fefed5c25b65263dc252ff6a8
Nikituh <aare.undo@gmail.com>
2020-04-07 11:03:41 +0200

    basic document scanning, image import (with scanbot picker)

--- Scanbot.SDK.Example.Forms/Scanbot.SDK.Example.Forms.iOS/AppDelegate.cs
+++ Scanbot.SDK.Example.Forms/Scanbot.SDK.Example.Forms.iOS/AppDelegate.cs
@@ -1,31 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 using Foundation;
+using ScanbotSDK.Xamarin.Forms;
 using UIKit;
 
 namespace Scanbot.SDK.Example.Forms.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
-    // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
+        // TODO Add the Scanbot SDK license key here.
+        // Please note: The Scanbot SDK will run without a license key for one minute per session!
+        // After the trial period is over all Scanbot SDK functions as well as the UI components
+        // will stop working or may be terminated. You can get an unrestricted
+        // "no-strings-attached" 30 day trial license key for free.
+        // Please submit the trial license form (https://scanbot.io/sdk/trial.html)
+        // on our website by using the app identifier
+        // "io.scanbot.example.sdk.barcode.xamarin.forms" of this example app.
+        const string LICENSE_KEY = null;
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            ImagePicker.Forms.iOS.DependencyManager.Register();
+
+            Console.WriteLine("Scanbot SDK Example: Initializing Scanbot SDK...");
+            SBSDKInitializer.Initialize(app, LICENSE_KEY, new SBSDKConfiguration
+            {
+                EnableLogging = true,
+                StorageBaseDirectory = GetDemoStorageBaseDirectory()
+            });
+
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+        }
+
+        string GetDemoStorageBaseDirectory()
+        {
+            // For demo purposes we use a sub-folder in the Documents folder in the
+            // Data Container of this App, since the contents can be shared via iTunes.
+            // For more detais about the iOS file system see:
+            // - https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html
+            // - https://docs.microsoft.com/en-us/xamarin/ios/app-fundamentals/file-system
+            // - https://docs.microsoft.com/en-us/dotnet/api/system.environment.specialfolder
+
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var customDocumentsFolder = Path.Combine(documents, "my-custom-storage");
+            Directory.CreateDirectory(customDocumentsFolder);
+
+            return customDocumentsFolder;
         }
     }
 }
--- Scanbot.SDK.Example.Forms/Scanbot.SDK.Example.Forms/MainPage.cs
+++ Scanbot.SDK.Example.Forms/Scanbot.SDK.Example.Forms/MainPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ScanbotSDK.Xamarin.Forms;
 using Xamarin.Forms;
 
@@ -7,6 +8,7 @@ namespace Scanbot.SDK.Example.Forms
     public class MainPage : ContentPage
     {
         StackLayout Container { get; set; }
+        Image Image { get; set; }
 
         public MainPage()
         {
@@ -16,22 +18,51 @@ namespace Scanbot.SDK.Example.Forms
             Container.Orientation = StackOrientation.Vertical;
             Container.BackgroundColor = Color.White;
 
+            Image = new Image
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.LightGray,
+                Aspect = Aspect.AspectFit
+            };
+            Image.SizeChanged += delegate
+            {
+                if (SelectedPage != null) {
+                    // Don't allow images larger than a third of the screen
+                    Image.HeightRequest = Content.Height / 3;
+                }
+            };
+            Container.Children.Add(Image);
+
             var table = new TableView();
             table.BackgroundColor = Color.White;
             Container.Children.Add(table);
 
             table.Root = new TableRoot();
-            table.Root.Add(new TableSection("Document Scanner")
+            table.Root.Add(new TableSection("SCAN DOCUMENTS")
             {
-                ViewUtils.CreateCell("SCANNING UI", ScanningUIClicked())
+                ViewUtils.CreateCell("SCANNING UI", ScanningUIClicked()),
+                ViewUtils.CreateCell("IMPORT IMAGE", ImportButtonClicked())
             });
-            table.Root.Add(new TableSection("Data Detectors")
+            table.Root.Add(new TableSection("DETECT DATA")
             {
                 ViewUtils.CreateCell("BASIC DATA DETECTOR", ScanningUIClicked())
             });
 
             Content = Container;
         }
+
+        IScannedPage _selectedPage;
+        public IScannedPage SelectedPage
+        {
+            get => _selectedPage;
+            set
+            {
+                _selectedPage = value;
+                Image.Source = _selectedPage.Original;
+            }
+        }
+
+        public List<IScannedPage> Pages { get; set; } = new List<IScannedPage>();
 
         EventHandler ScanningUIClicked()
         {
@@ -58,6 +89,25 @@ namespace Scanbot.SDK.Example.Forms
                     foreach (var page in result.Pages)
                         Pages.Add(page);
                     SelectedPage = Pages[0];
+                }
+            };
+        }
+
+        EventHandler ImportButtonClicked()
+        {
+            return async (sender, e) =>
+            {
+                if (!SDKUtils.CheckLicense(this)) { return; }
+
+                ImageSource source = await ImagePicker.Forms.ImagePicker.Instance.Pick();
+                if (source != null)
+                {
+                    // Import the selected image as original image and create a Page object
+                    var importedPage = await SBSDK.Operations.CreateScannedPageAsync(source);
+                    // Run document detection on it
+                    await importedPage.DetectDocumentAsync();
+                    SelectedPage = importedPage;
+                    Pages.Add(SelectedPage);
                 }
             };
         }

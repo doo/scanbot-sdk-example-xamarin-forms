8500c6c53725836e11624029a59121deafa9ff9c
Marco <marco@scanbot.io>
2021-03-16 17:52:45 +0100

    First implementation of Barcode Scanner Renderer example

---
+++ Native.Renderers.Example/Common/ScanbotSDKConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Native.Renderers.Example
+{
+    public class ScanbotSDKConfiguration
+    {
+#if DEBUG
+        public const bool IS_DEBUG = true;
+#else
+        public const bool IS_DEBUG = false;
+#endif
+
+        public const string LICENSE_KEY = "";
+    }
+}
---
+++ Native.Renderers.Example/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Native.Renderers.Example
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SetupViews();
+        }
+
+        private void SetupViews()
+        {
+            cameraView.OnBarcodeScanResult = (result) =>
+            {
+                string text = "Results: \n";
+                foreach (string barcodeText in result.Barcodes.Select((item) => item.Text))
+                {
+                    text += "\n" + barcodeText;
+                }
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    resultsLabel.Text = text;
+                });
+            };
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            start.Clicked += OnStartClick;
+            stop.Clicked += OnStopClick;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            start.Clicked -= OnStartClick;
+            stop.Clicked -= OnStopClick;
+        }
+
+        private void OnStartClick(object sender, EventArgs e)
+        {
+            cameraView.Resume();
+        }
+
+        private void OnStopClick(object sender, EventArgs e)
+        {
+            cameraView.Pause();
+        }
+    }
+}

8ae42747e861f74af0dc8a68ef3997ba6a77f0e2
Marco <marco@scanbot.io>
2021-08-27 11:48:20 +0200

    Xamarin.Essentials 1.6.1 -> 1.7.0

--- Native.Renderers.Example.Forms/Native.Renderers.Example.Forms.Android/Resources/Resource.designer.cs
+++ Native.Renderers.Example.Forms/Native.Renderers.Example.Forms.Android/Resources/Resource.designer.cs
@@ -10761,6 +10761,7 @@ namespace Native.Renderers.Example.Forms.Droid
 			global::Xamarin.Essentials.Resource.Id.text2 = global::Native.Renderers.Example.Forms.Droid.Resource.Id.text2;
 			global::Xamarin.Essentials.Resource.Id.time = global::Native.Renderers.Example.Forms.Droid.Resource.Id.time;
 			global::Xamarin.Essentials.Resource.Id.title = global::Native.Renderers.Example.Forms.Droid.Resource.Id.title;
+			global::Xamarin.Essentials.Resource.Id.view_tree_lifecycle_owner = global::Native.Renderers.Example.Forms.Droid.Resource.Id.view_tree_lifecycle_owner;
 			global::Xamarin.Essentials.Resource.Integer.status_bar_notification_info_maxnum = global::Native.Renderers.Example.Forms.Droid.Resource.Integer.status_bar_notification_info_maxnum;
 			global::Xamarin.Essentials.Resource.Layout.browser_actions_context_menu_page = global::Native.Renderers.Example.Forms.Droid.Resource.Layout.browser_actions_context_menu_page;
 			global::Xamarin.Essentials.Resource.Layout.browser_actions_context_menu_row = global::Native.Renderers.Example.Forms.Droid.Resource.Layout.browser_actions_context_menu_row;

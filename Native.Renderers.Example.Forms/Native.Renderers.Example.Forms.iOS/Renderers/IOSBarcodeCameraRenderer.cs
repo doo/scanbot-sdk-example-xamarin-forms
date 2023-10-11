cec0ca2fc4c273fdb27ae0f14fa75af80dc89fb8
Mayank <mayank@scanbot.io>
2023-10-11 11:40:01 +0200

    - [x] Removed the unused imports

--- Native.Renderers.Example.Forms/Native.Renderers.Example.Forms.iOS/Renderers/IOSBarcodeCameraRenderer.cs
+++ Native.Renderers.Example.Forms/Native.Renderers.Example.Forms.iOS/Renderers/IOSBarcodeCameraRenderer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Linq;
 using CoreGraphics;
-using GameController;
 using Native.Renderers.Example.Forms.iOS.Renderers;
 using Native.Renderers.Example.Forms.Views;
 using ScanbotSDK.iOS;

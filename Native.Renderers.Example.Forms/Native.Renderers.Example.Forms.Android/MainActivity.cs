5646e7dd939fc8acb7850b05ee862cb8d558bb62
Marko <marko@scanbot.io>
2023-11-09 15:58:33 +0100

    Update projects dependencies; Update Scanbot.Xamarin.Forms to v3.10.1-beta.1; Remove unnecessary dependencies and permissions; For Android projects update TargetFrameworkVersion to v13.0; Update Libraries.txt

--- Native.Renderers.Example.Forms/Native.Renderers.Example.Forms.Android/MainActivity.cs
+++ Native.Renderers.Example.Forms/Native.Renderers.Example.Forms.Android/MainActivity.cs
@@ -15,18 +15,11 @@ namespace Native.Renderers.Example.Forms.Droid
         {
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
             ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.Camera }, 0);
 
             LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }

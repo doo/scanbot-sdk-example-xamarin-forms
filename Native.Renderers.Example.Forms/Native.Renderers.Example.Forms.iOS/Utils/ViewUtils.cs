70cfe9967345f4c6fd16b159e5222f7758e90fbb
Mayank <mayank@scanbot.io>
2024-02-12 12:16:58 +0100

    Native Renderer - Updated the projects with latest SDK. Fixed the issues related to OverlayConfigurations.

---
+++ Native.Renderers.Example.Forms/Native.Renderers.Example.Forms.iOS/Utils/ViewUtils.cs
@@ -0,0 +1,24 @@
+﻿using UIKit;
+
+namespace Native.Renderers.Example.Forms.iOS.Utils
+{
+    public static class ViewUtils
+    {
+        /// <summary>
+        /// RootViewContoller from the view hierarchy.
+        /// </summary>
+        internal static UIViewController RooViewController => (UIApplication.SharedApplication?.Delegate as AppDelegate)?.Window?.RootViewController;
+
+        // ------------------------------------------------------------------------------------------------------------------------
+        // Displays a popup message with message and a single button.s
+        // ------------------------------------------------------------------------------------------------------------------------
+        internal static void ShowAlert(string message, string buttonTitle)
+        {
+            var alert = UIAlertController.Create("Alert", message, UIAlertControllerStyle.Alert);
+            var action = UIAlertAction.Create(buttonTitle ?? "Ok", UIAlertActionStyle.Cancel, (obj) => { });
+            alert.AddAction(action);
+            RooViewController?.PresentViewController(alert, true, null);
+        }
+    }
+}
+
